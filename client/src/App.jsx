dfdba0c5be22c496806e8c6e9c11e976c201e905
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-09-23 00:26:20 +0530

    Setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-2xl font-bold text-red-500 pl-70 pt-70 '>Starting the frontend of the project Genova Ai</h1>
+    </>
+  )
+}
+
+export default App
