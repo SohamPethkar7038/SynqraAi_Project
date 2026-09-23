f165acf908d295cf4d0d1a622424dbd3acb06961
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-09-24 00:18:53 +0530

    Set up Clerk authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import {ClerkProvider} from '@clerk/clerk-react'
 import './index.css'
 import App from './App.jsx'
 
+// importing clerk publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable key')
+}
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>,
+  </ClerkProvider>
 )
